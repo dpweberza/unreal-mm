3b8772387cbb96c34e2dfbc5f55b6c05c0f418f6
David Weber <dpweberza@gmail.com>
2023-06-23 13:28:44 +1000

    first commit

---
+++ Plugins/mmbp/Source/mmbp/Private/mmbpBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "mmbpBPLibrary.h"
+#include "mmbp.h"
+
+UmmbpBPLibrary::UmmbpBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UmmbpBPLibrary::mmbpSampleFunction(float Param)
+{
+	return -1;
+}
+
